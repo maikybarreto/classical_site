deb6e0253fb819053036eb9bb14f261b37295aa2
maikybarreto <maikybarreto@icloud.com>
2022-01-17 22:16:21 -0300

    Atualizações no JS. Adição do grid na terceira página.

--- js/curriculum.js
+++ js/curriculum.js
@@ -1,9 +1,31 @@
 let author_name_run = false;
 
+// Função de delay para uso interno nas funções. Necessário utilizar async nas funções.
 async function sleep(millis) {
     return new Promise(resolve => setTimeout(resolve, millis));
 }
 
+// Função base para a simulação de digitação de caracteres.
+async function char_by_char_text(string, element,delay, post_delay = 0, post_function) {
+    let current_string = '';
+    for(let c in string) {
+        current_string += string[c];
+        element.innerHTML = current_string;
+        await sleep(delay);
+    }
+    await sleep(post_delay);
+    post_function()
+}
+
+// Função base para o aparecimento ou sumiço de um elemento por meio do atributo opacity.
+async function show_text(element, delay) {
+    for(let i = 0; i <= 10; i ++) {
+        element.style.opacity = i/10;
+        await sleep(delay);
+    }
+}
+
+// Função personalizada para as aplicações da página.
 async function author_name(){
     let author_name = "Maiky Barreto da Silva";
     let text_space = document.getElementById("author_name");
@@ -17,7 +39,6 @@ async function author_name(){
     }
     await sleep(200);
 
-    // document.getElementById("subtitle").style.opacity = 0.5;
     subtitle = document.getElementById("subtitle");
     for(let i = 0; i <= 10; i ++) {
         subtitle.style.opacity = i/10;
